b2798a47fb97ee0ee49a257d5e8660a2a18969b3
Madelyn Kasula <maddie.kasula@code.org>
2021-11-24 18:52:23 -0800

    Add onClick prop to Button; don't guard against falsy value

--- packages/cl-test-button/src/Button.jsx
+++ packages/cl-test-button/src/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Text from "@madkas/cl-test-text";
 
-export default function Button({ text }) {
+export default function Button({ text, onClick }) {
+  // Purposefully don't guard against falsy onClick prop for testing
   return (
-    <button>
+    <button onClick={onClick}>
       <Text>{text}</Text>
     </button>
   );

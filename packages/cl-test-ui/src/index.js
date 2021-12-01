80b7803a60cf33752895d58b5ecd69a871953c98
Madelyn Kasula <maddie.kasula@code.org>
2021-12-01 13:02:45 -0800

    Add cl-test-ui with very basic theme object

---
+++ packages/cl-test-ui/src/index.js
@@ -0,0 +1,12 @@
+// TODO: create/load gotham
+
+export const theme = {
+  default: {
+    primaryBgColor: "transparent",
+    primaryFontColor: "cadetblue",
+  },
+  dark: {
+    primaryBgColor: "black",
+    primaryFontColor: "white",
+  },
+};

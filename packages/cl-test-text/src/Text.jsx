697774d0ab5bbaff7052d4b935490b7b3e67421a
Madelyn Kasula <maddie.kasula@code.org>
2021-12-16 16:36:30 -0800

    Don't build as part of  script

--- packages/cl-test-text/src/Text.jsx
+++ packages/cl-test-text/src/Text.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import style from "./text.module.scss";
 
 export default function Text({ element, text, newProp }) {
-  if (!newProp) {
-    throw new Error("hmmm how bout now");
-  }
-
   const Element = element || "span";
   return <Element className={style.text}>{text}</Element>;
 }

94bfaf4305905721ca42d9e117dd5ab06ec9cf16
Madelyn Kasula <maddie.kasula@code.org>
2021-12-17 12:29:19 -0800

    Remove unused prop

--- packages/cl-test-text/src/Text.jsx
+++ packages/cl-test-text/src/Text.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./text.module.scss";
 
-export default function Text({ element, text, newProp }) {
+export default function Text({ element, text }) {
   const Element = element || "span";
   return <Element className={style.text}>{text}</Element>;
 }
